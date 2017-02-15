08a3b4519b632b153b089982eab2a818672ee4f2
bloosnail <dah124@pitt.edu>
2017-02-15 03:25:50 -0500

    Create validate.js

---
+++ JS/validate.js
@@ -0,0 +1,16 @@
+function checkName(event){
+  name = document.getElementById("yourName").value;
+	
+  if(/^[a-zA-Z]+$/.test(name)){
+    text = "Wow " + name + " is a great name! Good job!";
+	
+	var f = document.getElementById("myForm");
+	f.parentNode.removeChild(f);
+	
+    }else{
+      text = "You have to have a name and it can only have letters (sorry yours is invalid)."
+    }
+	
+  document.getElementById("display").innerHTML = text;
+	
+}
