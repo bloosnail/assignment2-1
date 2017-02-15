ad51c45b688ae8197dab95bb8bc106bd3827ec45
bloosnail <dah124@pitt.edu>
2017-02-15 03:29:44 -0500

    Update validate.js

--- JS/validate.js
+++ JS/validate.js
@@ -2,10 +2,9 @@ function checkName(event){
   name = document.getElementById("yourName").value;
 	
   if(/^[a-zA-Z]+$/.test(name)){
-    text = "Wow " + name + " is a great name! Good job!";
-	
-	var f = document.getElementById("myForm");
-	f.parentNode.removeChild(f);
+    text = "Wow " + name + " is a great name! Good job!";	
+    var f = document.getElementById("myForm");
+    f.parentNode.removeChild(f);
 	
     }else{
       text = "You have to have a name and it can only have letters (sorry yours is invalid)."
